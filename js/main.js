08747b10a934c8d0bee5666948286b64a0cf7738
Alejandro Miranda <ale.miranda4503@gmail.com>
2022-05-05 02:15:36 -0300

    nuevo

--- js/main.js
+++ js/main.js
@@ -114,7 +114,7 @@ const checkRow = () => {
             if (currentRow >= 5) {
                 isGameOver = false
                 showMessage('Ups, perdiste mal ahí :(')
-                showMessage('La cuenta es "56/8+2=1"')
+                showMessage('La cuenta es "56/8+2=5"')
                 return
             }
             if (currentRow < 5) {
